70414c6daef84e141afaa591edf6eae7cd85fc76
boldham31 <bryceoldham@gmail.com>
2021-12-21 23:26:26 -0800

    adds scss

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import './App.css';
+import './Base.scss';
 import Card from '@material-ui/core/Card';
 import Typography from '@material-ui/core/Typography';
 import CardContent from '@material-ui/core/CardContent';
